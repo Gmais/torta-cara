692ba03875c2e5e59dbdce2407ff02cffd0ef1ee
Gilson <apcturvo@gmail.com>
2026-07-24 18:17:57 -0300

    fix: extend HTMLAttributes on CardProps to support onClick

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-interface CardProps {
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
 }
 
-export function Card({ children, className, style }: CardProps) {
+export function Card({ children, className, style, ...props }: CardProps) {
   return (
-    <div className={`${styles.card} ${className || ''}`} style={style}>
+    <div className={`${styles.card} ${className || ''}`} style={style} {...props}>
       {children}
     </div>
   );
